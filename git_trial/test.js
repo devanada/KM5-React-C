be5f17a54b279e930508ea3bfea386d17f2ce503
Yoga S Devanada <ydevanada@gmail.com>
2023-08-16 15:21:59 +0700

    fix: add comments for better readability

--- git_trial/test.js
+++ git_trial/test.js
@@ -1,3 +1,9 @@
+/**
+ * Returns summary of a and b
+ *
+ * @param {number} a First Parameter
+ * @param {number} b Second Parameter
+ */
 function sumAB(a, b) {
   const parseA = parseInt(a);
   const parseB = parseInt(b);
